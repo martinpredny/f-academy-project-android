29f4a5443e56ddf0c139db238675d9860258448c
martin_predny <martin.predny@dactylgroup.com>
2023-08-01 13:32:10 +0200

    Task 3 - santa and goblins implemented.

--- app/src/test/kotlin/app/futured/academyproject/Task3Test.kt
+++ app/src/test/kotlin/app/futured/academyproject/Task3Test.kt
@@ -1,15 +1,28 @@
 package app.futured.academyproject
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
+import kotlin.random.Random
 
 class Task3Test {
 
     @Test
-    fun santaAndGoblins() {
-        /**
-         * Santa ma na starosti 10 skritku
-         * Kazdy skritek vykonava nejakou praci od 100 ms po 1 s
-         * Santa by mel pockat az skritci dokonci svoji praci a potom teprve muze ukoncit svoji
-         */
+    fun santaAndGoblins() = runTest {
+        val myFlow = flow {
+            for (i in 1..10) {
+                val randomDwarfTaskTime = Random.nextLong(100, 1001)
+                delay(randomDwarfTaskTime)
+                emit("Dwarf $i finished his work")
+            }
+        }
+        this.launch {
+            myFlow.collect { result ->
+                println(result)
+            }
+            println("Now santa can finish his work")
+        }
     }
 }
