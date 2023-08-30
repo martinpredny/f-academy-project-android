643f7071e9f17333fc9993cfe5519b35d23af35e
martin_predny <martin.predny@dactylgroup.com>
2023-08-30 15:47:24 +0200

    Implemented hiding of top and bottom bar.

--- app/src/main/kotlin/app/futured/academyproject/ui/screens/main/MainScreen.kt
+++ app/src/main/kotlin/app/futured/academyproject/ui/screens/main/MainScreen.kt
@@ -12,10 +12,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import app.futured.academyproject.data.model.local.BottomNavigationItem
 import app.futured.academyproject.navigation.NavigationDestinations
@@ -33,6 +38,15 @@ fun MainScreen(
     navigation: NavigationDestinations = remember { NavigationDestinationsImpl(navController) },
 ) {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    var showBottomAndTopBar by rememberSaveable { mutableStateOf(true) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    showBottomAndTopBar = when (navBackStackEntry?.destination?.route) {
+        "Culture" -> true
+        "Tourism" -> true
+        "Events" -> true
+        else -> false
+    }
 
     val items = listOf(
         BottomNavigationItem(
@@ -61,13 +75,17 @@ fun MainScreen(
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(scrollBehavior)
+            if(showBottomAndTopBar) {
+                TopAppBar(scrollBehavior)
+            }
         },
         content = {
             NavGraph(navController, navigation, it)
         },
         bottomBar = {
-            BottomNavigationBar(items = items, navigation = navigation)
+            if(showBottomAndTopBar) {
+                BottomNavigationBar(items = items, navigation = navigation)
+            }
         },
     )
 }
