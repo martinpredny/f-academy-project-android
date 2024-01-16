3bcd8011adeda3598189f25b550ac507a68d3304
martin_predny <martin.predny@dactylgroup.com>
2024-01-16 14:05:20 +0100

    Externalise strings in MenuItems.kt into constant variables.

--- app/src/main/kotlin/app/futured/academyproject/ui/MenuItems.kt
+++ app/src/main/kotlin/app/futured/academyproject/ui/MenuItems.kt
@@ -18,20 +18,42 @@ import androidx.compose.material.icons.outlined.Tour
 import app.futured.academyproject.data.model.local.menus.BottomNavigationItem
 import app.futured.academyproject.data.model.local.menus.DrawerNavigationItem
 import app.futured.academyproject.data.model.local.menus.TabItem
+import app.futured.academyproject.navigation.DestinationRoutes.ABOUT
+import app.futured.academyproject.navigation.DestinationRoutes.CULTURE
+import app.futured.academyproject.navigation.DestinationRoutes.LOGIN
+import app.futured.academyproject.ui.SectionTitles.ABOUT_TITLE
+import app.futured.academyproject.ui.SectionTitles.CULTURE_TITLE
+import app.futured.academyproject.ui.SectionTitles.DISCOVER_TITLE
+import app.futured.academyproject.ui.SectionTitles.EVENTS_TITLE
+import app.futured.academyproject.ui.SectionTitles.INFO_TITLE
+import app.futured.academyproject.ui.SectionTitles.LOGIN_TITLE
+import app.futured.academyproject.ui.SectionTitles.MAP_TITLE
+import app.futured.academyproject.ui.SectionTitles.TOURISM_TITLE
+
+object SectionTitles {
+    const val CULTURE_TITLE = "Culture"
+    const val TOURISM_TITLE = "Tourism"
+    const val EVENTS_TITLE = "Events"
+    const val DISCOVER_TITLE = "Discover"
+    const val ABOUT_TITLE = "About"
+    const val LOGIN_TITLE = "Log In"
+    const val INFO_TITLE = "Info"
+    const val MAP_TITLE = "Show On Map"
+}
 
 val bottomNavBarItems = listOf(
     BottomNavigationItem(
-        title = "Culture",
+        title = CULTURE_TITLE,
         selectedIcon = Icons.Filled.AccountBalance,
         unselectedIcon = Icons.Outlined.AccountBalance,
     ),
     BottomNavigationItem(
-        title = "Tourism",
+        title = TOURISM_TITLE,
         selectedIcon = Icons.Filled.Tour,
         unselectedIcon = Icons.Outlined.Tour,
     ),
     BottomNavigationItem(
-        title = "Events",
+        title = EVENTS_TITLE,
         selectedIcon = Icons.Filled.Event,
         unselectedIcon = Icons.Outlined.Event,
     ),
@@ -39,33 +61,33 @@ val bottomNavBarItems = listOf(
 
 val drawerItems = listOf(
     DrawerNavigationItem(
-        title = "Discover",
+        title = DISCOVER_TITLE,
         selectedIcon = Icons.Filled.Public,
         unselectedIcon = Icons.Outlined.Public,
-        route = "Culture",
+        route = CULTURE,
     ),
     DrawerNavigationItem(
-        title = "About",
+        title = ABOUT_TITLE,
         selectedIcon = Icons.Filled.Info,
         unselectedIcon = Icons.Outlined.Info,
-        route = "About",
+        route = ABOUT,
     ),
     DrawerNavigationItem(
-        title = "Log In",
+        title = LOGIN_TITLE,
         selectedIcon = Icons.Filled.Login,
         unselectedIcon = Icons.Outlined.Login,
-        route = "Login",
+        route = LOGIN,
     ),
 )
 
 val tabItems = listOf(
     TabItem(
-        title = "Info",
+        title = INFO_TITLE,
         selectedIcon = Icons.Filled.Info,
         unselectedIcon = Icons.Outlined.Info,
     ),
     TabItem(
-        title = "Show On Map",
+        title = MAP_TITLE,
         selectedIcon = Icons.Filled.Map,
         unselectedIcon = Icons.Outlined.Map,
     ),
